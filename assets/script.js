61691bf411bf156fa744020ee8de6debbc8848b1
Shrishail Bagale <shrishailbagale3@gmail.com>
2024-05-27 16:06:43 +0530

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+function copyLink() {
+    const dummy = document.createElement('input');
+    const text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+
+    alert('Link copied to clipboard!');
+}
